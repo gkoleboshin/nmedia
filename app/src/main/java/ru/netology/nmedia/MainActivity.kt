519254e668068e5d7e46bc27761d39561d94c187
Gennadiy Koleboshin <gk4521@gmail.ru>
2022-04-11 01:44:10 +0300

    add CRUD

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,10 +1,14 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.util.hideKeyboard
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +18,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostsAdapter ({
-            viewModel.shareById(it.id)
-        },{
-            viewModel.likeById(it.id)
-        })
+        val adapter = PostsAdapter(viewModel)
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
+        }
+        val group: Group = findViewById(R.id.groupIsNotVisible)
+        group.visibility = View.GONE
+        viewModel.editPost.observe(this) { post: Post? ->
+            val content = post?.content ?: ""
+            group.visibility = View.VISIBLE
+            binding.contentEditText.setText(content)
+            group.setVisibility(View.VISIBLE)
+        }
+        binding.cancelButton.setOnClickListener {
+            with(binding.contentEditText) {
+                setText("")// TODO так делать не надо
+                clearFocus()
+                hideKeyboard()
+                group.visibility =View.GONE
+            }
+        }
+        binding.saveButton.setOnClickListener {
+            with(binding.contentEditText) {
+                val content = text.toString()
+                viewModel.onSaveButtonClicked(content)
+                setText("")// TODO так делать не надо
+                clearFocus()
+                hideKeyboard()
+                group.visibility =View.GONE
+            }
         }
     }
 }
--- app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
@@ -1,13 +1,50 @@
 package ru.netology.nmedia.viewmodel
 
+import androidx.constraintlayout.widget.Group
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import ru.netology.nmedia.adapter.PostInteractionListiner
+import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
 
-class PostViewModel : ViewModel() {
-    // упрощённый вариант
+class PostViewModel : ViewModel(), PostInteractionListiner {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
-    val data = repository.getAll()
-    fun likeById(id: Long) = repository.likeById(id)
-    fun shareById(id:Long) = repository.shareById(id)
+    val data by repository::data
+    val editPost = MutableLiveData<Post?>()
+    fun onSaveButtonClicked(postContent: String) {
+        val updatedPost = editPost.value?.copy(
+            content = postContent
+        ) ?: Post(
+            id = 0L,
+            author = "",
+            content = postContent,
+            published = "",
+            likedByMe = false
+        )
+        repository.save(updatedPost)
+        this.editPost.value = null
+    }
+
+    // region PostInteractionListiner implmentation
+
+    override fun onLike(post: Post) {
+        repository.likeById(post.id)
+    }
+
+    override fun onShare(post: Post) {
+        repository.shareById(post.id)
+    }
+
+    override fun onRemove(post: Post) {
+        repository.removeById(post.id)
+    }
+
+    override fun onEdit(post: Post) {
+        editPost.value = post
+    }
+
+    // endregion PostInteractionListiner implmentation
+
 }
